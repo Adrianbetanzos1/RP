39a4639d97c2ea36adaed024b2048cf0ea073ca9
Adrianbetanzos1 <adrianbetanzos15@hotmail.com>
2023-11-23 13:30:38 -0600

    Aplicacion

---
+++ RP/Data/ApplicationDbContext.cs
@@ -0,0 +1,5 @@
+namespace RP.Data;
+
+public class ApplicationDbContext{
+    
+}
---
+++ RP/Models/BooksEntity.cs
@@ -0,0 +1,9 @@
+namespace RP.Models;
+
+public class BooksEntity{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    
+}

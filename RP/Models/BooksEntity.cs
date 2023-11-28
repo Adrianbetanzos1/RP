6c3ef6e38664d8a7e8e2fe484d8c1214a7ba6178
Adrianbetanzos1 <adrianbetanzos15@hotmail.com>
2023-11-28 10:08:21 -0600

    Cambios puntuales

--- RP/Models/BooksEntity.cs
+++ RP/Models/BooksEntity.cs
@@ -2,8 +2,8 @@ namespace RP.Models;
 
 public class BooksEntity{
     public int ID { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public string ISBN { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? ISBN { get; set; }
     
 }
